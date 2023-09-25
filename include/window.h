2a9504ee72c3f678897afd9eb5e71db8f1df486f
rhysqi <rhysqi.akbar@gmail.com>
2023-09-25 17:37:46 +0700

    Update window.h

--- include/window.h
+++ include/window.h
@@ -1,10 +1,31 @@
+
+#include <X11/Xlib.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+inline void OWindow(Display *Dsp, int Scr, int Root, int x, int y, int height, int width, const char *BG) {
+    // XCreate Window (Simple)
+    int Black = BlackPixel(Dsp, Scr);
+    int White = WhitePixel(Dsp, Scr);
+
+	if (strcmp(BG, "black")) {
+	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, Black, White);
+	    XMapWindow(Dsp, Win);
+	} else {
+	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, White, Black);
+	    XMapWindow(Dsp, Win);
+	}
+}
+
+
+// Data
 #ifndef DATA_H
 #define DATA_H
 
 #define MAX_CHAR    200
 #define WINDOW_NAME "Window"
 
-#include <X11/Xlib.h>
 
 typedef struct
 {
@@ -30,23 +51,3 @@ typedef struct
 } WinConf;
 
 #endif // !DATA_H
-
-#include <X11/Xlib.h>
-
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-
-inline void OWindow(Display *Dsp, int Scr, int Root, int x, int y, int height, int width, const char *BG) {
-    // XCreate Window (Simple)
-    int Black = BlackPixel(Dsp, Scr);
-    int White = WhitePixel(Dsp, Scr);
-
-	if (strcmp(BG, "black")) {
-	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, Black, White);
-	    XMapWindow(Dsp, Win);
-	} else {
-	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, White, Black);
-	    XMapWindow(Dsp, Win);
-	}
-}
