c8dd74695cfb287a8369d8aa47cc1e4a278751af
rhysqi <rhysqi.akbar@gmail.com>
2023-09-25 17:34:51 +0700

    Update header

--- include/system.h
+++ include/system.h
@@ -3,21 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-inline void Loop(Display *Dsp, int Scr) {
+inline void Check(Display *Dsp, int Scr) {
     Dsp = XOpenDisplay(NULL);
     Scr = DefaultScreen(Dsp);
 
-    if (Dsp == NULL)
-	{
+	if (Dsp == NULL) {
 	    printf("No display detected !\n");
 	    exit(1);
     }
 }
 
-inline void EndLoop(Display *Dsp) { XCloseDisplay(Dsp); }
+inline void Loop(Display *Dsp, XEvent Xe) {
+	for (;;) {
+	    XNextEvent(Dsp, &Xe);
+	}
+}
 
-inline void WMapping(Display *Dsp, Window Win) { XMapWindow(Dsp, Win); }
+inline void Close(Display *Dsp) {
+    XCloseDisplay(Dsp);
+}
 
-inline void UnMapping(Display *Dsp, Window Win) { XUnmapWindow(Dsp, Win); }
+inline void WMapping(Display *Dsp, Window Win) {
+    XMapWindow(Dsp, Win);
+}
 
-inline void DestroyWin(Display *Dsp, Window Win) { XDestroyWindow(Dsp, Win); }
+inline void UnMapping(Display *Dsp, Window Win) {
+    XUnmapWindow(Dsp, Win);
+}
+
+inline void DestroyWin(Display *Dsp, Window Win) {
+    XDestroyWindow(Dsp, Win);
+}
--- include/window.h
+++ include/window.h
@@ -1,3 +1,35 @@
+#ifndef DATA_H
+#define DATA_H
+
+#define MAX_CHAR    200
+#define WINDOW_NAME "Window"
+
+#include <X11/Xlib.h>
+
+typedef struct
+{
+    Display *Dsp;
+    Window   Win;
+    int	     Scr;
+    XEvent   Xe;
+} DspConf;
+
+typedef struct
+{
+    float X;
+    float Y;
+    float HEIGHT;
+    float WIDTH;
+    char *NAME;
+    int	  Root;
+
+    int			 ColMap;
+    Visual		*visual;
+    int			 Depth;
+    XSetWindowAttributes Attr;
+} WinConf;
+
+#endif // !DATA_H
 
 #include <X11/Xlib.h>
 
@@ -5,23 +37,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-// XCreate Window (Advanced)
-inline void RH_Window() {}
-
-// XCreate Window (Simple)
-inline void RH_Simp_Window(Display *Dsp, int Scr, int Root, int x, int y,
-			   int height, int width, const char *BG) {
+inline void OWindow(Display *Dsp, int Scr, int Root, int x, int y, int height, int width, const char *BG) {
+    // XCreate Window (Simple)
     int Black = BlackPixel(Dsp, Scr);
     int White = WhitePixel(Dsp, Scr);
-    if (strcmp(BG, "black"))
-	{
-	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1,
-					     Black, White);
+
+	if (strcmp(BG, "black")) {
+	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, Black, White);
 	    XMapWindow(Dsp, Win);
-    } else
-	{
-	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1,
-					     White, Black);
+	} else {
+	    Window Win = XCreateSimpleWindow(Dsp, Root, x, y, width, height, 1, White, Black);
 	    XMapWindow(Dsp, Win);
 	}
 }
