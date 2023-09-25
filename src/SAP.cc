50dfb73e91898d52cef1e1b2ae5ec4dba5500a59
rhysqi <rhysqi.akbar@gmail.com>
2023-09-25 17:35:02 +0700

    Update main files

--- src/SAP.cc
+++ src/SAP.cc
@@ -4,6 +4,20 @@
 
 int main(int argc, const char *argv[]) {
 
-    printf("hello\n");
+    DspConf DConf;
+    WinConf WConf;
+    Window  Win1;
+
+    Display *Dsp1  = XOpenDisplay(NULL);
+    int	     Scr1  = DConf.Scr;
+    int	     Root1 = RootWindow(Dsp1, Scr1);
+
+    Check(Dsp1, Scr1);
+
+    OWindow(Dsp1, Scr1, Root1, 0, 0, 500, 800, "white");
+
+    Loop(Dsp1, DConf.Xe);
+    Close(Dsp1);
+
     return 0;
 }
