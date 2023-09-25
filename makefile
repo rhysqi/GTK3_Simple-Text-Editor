6528c7f9cdcda3db613ec77e506d7e1a9c2727b1
rhysqi <rhysqi.akbar@gmail.com>
2023-09-25 16:40:45 +0700

    update main

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ Files = src/*.cc -o
 Output = build/Program
 
 LIBS = -L/usr/local/lib -I/usr/local/include
-LDFLAGS= -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system
+LDFLAGS= -lX11
 
 OPT1 = -O3 -std=c++20 -stdlib=libc++ -target x86_64-pc-freebsd13.2 -H 
 OPT2 = -flto -fvectorize -fstack-protector-strong -fslp-vectorize -v
--- src/SAP.cc
+++ src/SAP.cc
@@ -1,41 +1,7 @@
 
-#include "../include/SAP.hh"
-#include "SFML/Audio/Sound.hpp"
-#include <stdio.h>
 
-int main() {
-    // Inisialisasi SFML
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Audio");
-    sf::Music music;
-    sf::Sound sound;
+int main(int argc, const char *argv[]){
 
-    // Load music
-    if (!music.openFromFile("../sample.ogg")) {
-        printf("Failed to load audio file");
-        return 1;
-    }
-
-    // Putar audio
-    music.setVolume(10.0f);
-    music.play();
-
-    // Loop utama
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        // Tambahan: Hentikan audio jika tombol Escape ditekan
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            music.stop();
-        }
-
-        window.clear();
-        window.display();
-    }
-
-    return 0;
+  
+  return 0;
 }
