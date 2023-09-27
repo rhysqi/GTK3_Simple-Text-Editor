b9d250889031f8e992a5ddd6cc941864415e7132
rhysqi <rhysqi.akbar@gmail.com>
2023-09-27 23:26:57 +0700

    update function and component

---
+++ include/component.h
@@ -0,0 +1,66 @@
+
+#include <gtk-3.0/gdk/gdk.h>
+#include <gtk-3.0/gtk/gtk.h>
+
+// Menu bar Function
+void on_new_clicked(GtkWidget *widget, gpointer data) {
+    // Tambahkan logika untuk tindakan "New" di sini
+    g_print("New button clicked\n");
+}
+
+// Fungsi yang akan dipanggil ketika tombol "Open" diklik
+void on_open_clicked(GtkWidget *widget, gpointer data) {
+    // Tambahkan logika untuk tindakan "Open" di sini
+    g_print("Open button clicked\n");
+}
+
+void on_save_clicked(GtkWidget *widget, gpointer data) {
+    // Tambahkan logika untuk tindakan "Save" di sini
+    g_print("Save button clicked\n");
+}
+
+inline void menubar(GtkWidget *window, GtkWidget *box) {
+
+    GtkWidget *menu;
+    menu = gtk_menu_bar_new();
+
+    GtkWidget *menu_item_new;
+    menu_item_new = gtk_menu_item_new_with_label("New");
+
+    GtkWidget *menu_item_open;
+    menu_item_open = gtk_menu_item_new_with_label("Open");
+
+    GtkWidget *menu_item_save;
+    menu_item_save = gtk_menu_item_new_with_label("Save");
+
+    // Menu bar show
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item_new);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item_open);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item_save);
+    gtk_container_add(GTK_CONTAINER(window), box);
+    gtk_box_pack_start(GTK_BOX(box), menu, FALSE, FALSE, 0);
+
+    // signal connect
+    g_signal_connect(menu_item_new, "activate", G_CALLBACK(on_new_clicked), NULL);
+    g_signal_connect(menu_item_new, "activate", G_CALLBACK(on_open_clicked), NULL);
+    g_signal_connect(menu_item_new, "activate", G_CALLBACK(on_new_clicked), NULL);
+}
+
+// Entry Function
+inline void entry_activated(GtkEntry *entry) {
+    const gchar *text = gtk_entry_get_text(entry);
+    g_print("Teks Masuk: %s\n", text);
+}
+
+inline void entry(GtkWidget *window, GtkWidget *box) {
+     // Membuat GtkScrolledWindow
+    GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_box_pack_start(GTK_BOX(box), scrolled_window, TRUE, TRUE, 0);
+
+    // Membuat GtkTextView
+    GtkWidget *entry = gtk_text_view_new();
+    g_signal_connect(entry, "activate", G_CALLBACK(entry_activated), NULL);
+    
+    // Mengatur scroll untuk GtkScrolledWindow
+    gtk_container_add(GTK_CONTAINER(scrolled_window), entry);
+}
--- include/window.h
+++ include/window.h
@@ -2,5 +2,7 @@
 #include <gtk-3.0/gdk/gdk.h>
 #include <gtk-3.0/gtk/gtk.h>
 
-inline void Window() {
+inline void Window(GtkWidget *window) {
+    gtk_window_set_title(GTK_WINDOW(window), "Simple Text Editor");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 500);
 }
