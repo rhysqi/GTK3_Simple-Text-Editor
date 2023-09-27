9a2dc4ac653d70d447fa7e9834bc92b2e8afe4d4
rhysqi <rhysqi.akbar@gmail.com>
2023-09-28 02:00:00 +0700

    update header file

--- include/component.h
+++ include/component.h
@@ -3,18 +3,22 @@
 #include <gtk-3.0/gtk/gtk.h>
 #include <stdio.h>
 
+GtkTextBuffer *text_buffer = NULL;
+
 // Menu bar Function
-inline void on_new_clicked(GtkWidget *widget, gpointer data) {
-    printf("Hello\n");
+inline void on_new_clicked(GtkWidget *widget) {
+    // new files function
+    if (text_buffer != NULL) {
+        gtk_text_buffer_set_text(text_buffer, "", -1);
+    }
 }
 
-// Fungsi yang akan dipanggil ketika tombol "Open" diklik
-inline void on_open_clicked(GtkWidget *widget, gpointer data) {
-
+inline void on_open_clicked(GtkWidget *widget) {
+    // Open file function
 }
 
-inline void on_save_clicked(GtkWidget *widget, gpointer data) {
-    
+inline void on_save_clicked(GtkWidget *widget) {
+    // save file function
 }
 
 inline void menubar(GtkWidget *window, GtkWidget *box) {
@@ -51,14 +55,17 @@ inline void entry_activated(GtkEntry *entry) {
 }
 
 inline void entry(GtkWidget *window, GtkWidget *box) {
-     // Membuat GtkScrolledWindow
+    // Membuat GtkScrolledWindow
     GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_box_pack_start(GTK_BOX(box), scrolled_window, TRUE, TRUE, 0);
 
     // Membuat GtkTextView
     GtkWidget *entry = gtk_text_view_new();
-    g_signal_connect(entry, "activate", G_CALLBACK(entry_activated), NULL);
-    
+    g_signal_connect(entry, "activated", G_CALLBACK(entry_activated), entry);
+
     // Mengatur scroll untuk GtkScrolledWindow
     gtk_container_add(GTK_CONTAINER(scrolled_window), entry);
+
+    // Menginisialisasi buffer teks
+    text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(entry));
 }
